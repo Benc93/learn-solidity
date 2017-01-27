2128dcbd22884b4fa2fd4f5452fdb7456e0fae1a
Ben Conway <bendavidconway@me.com>
2017-01-27 21:20:41 +0700

    commented out, waiting for testnet to sync

--- example.sol
+++ example.sol
@@ -1,3 +1,6 @@
+// First Contract to be committed to the Ethereum testnet. 
+// A simple contract that allows a user to define the name of a company and record the naming on the blockchain.
+
 contract Example{
 
     string public brandName;
