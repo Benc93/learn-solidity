45a8461b6833516eda502d9e387fb4a6c7ce71d8
Ben Conway <bendavidconway@me.com>
2017-01-28 18:10:02 +0700

    Added first contract, added compiler requirement to example.sol, added exmaple2.sol

--- example.sol
+++ example.sol
@@ -1,6 +1,9 @@
 // First Contract to be committed to the Ethereum testnet. 
 // A simple contract that allows a user to define the name of a company and record the naming on the blockchain.
 
+// must include the version requirement in order for contract code to compile
+pragma solidity ^0.4.6;
+
 contract Example{
 
     string public brandName;
---
+++ example2.sol
@@ -0,0 +1,12 @@
+// Second contract pushed to the testnet blockchain
+// This covers simple variables in Solidity
+
+// must include the version requirement in order for contract code to compile
+pragma solidity ^0.4.6;
+
+contract Example2{
+
+	string public one = "one";
+	int public two = 2;
+	uint public three = 3;
+}
