49c7f2c7788b060ef553b8eaca7322ced0885811
Ben Conway <bendavidconway@me.com>
2017-02-08 22:27:14 +0700

    two contracts, one inherits a string from the other

---
+++ Inheritance/simpleInheritanceExample.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.6;
+
+contract ownedByCryptoUni{
+	string public dudesName = "Crypto Uni"
+}
+
+contract ethereumCourse is ownedByCryptoUni{
+
+	struct Student{
+		string studentName;
+		uint studentAge;
+		bool active;
+
+	}
+
+	uint256 public fee;
+
+	mapping(address=>student) public myStudents;
+
+	// 1. Register to my school
+	// 2. Set fee for registration
+
+	function Register(
+		string: _typeYourName,
+		uint: _whatIsYourAge,){
+
+		if (msg.value == fee){
+		// register the student
+		myStudents[msg.sender] = student({
+			studentName: _typeYourName,
+			studentAge: _whatIsYourAge,
+			active: true
+			});		
+		}else{
+			throw;
+		}
+
+	}
+
+	function setRegistrationFee(uint256 _fee){
+		fee = _fee;
+	}
+
+}
