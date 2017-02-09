d886fbb69fdfcd5fb7bfd8d21e68ac3205c03058
Ben Conway <bendavidconway@me.com>
2017-02-09 19:26:57 +0700

    created parent and child contracts where the parent passes various data to the child, strings and integers mostly

--- Proxy Contracts/parentAndChildContractInteracting.sol
+++ Proxy Contracts/parentAndChildContractInteracting.sol
@@ -3,9 +3,19 @@ pragma solidity ^0.4.6;
 contract child {
 
 	string public childName;
+	uint256 public childAge;
+	string public childLocation;
 
 	function setChildName (string _name){
 		childName = _name;
+	}
+
+	function setChildAge (uint256 _age){
+		childAge = _age;
+	}
+
+	function setChildLocation (string _location){
+		childLocation = _location;
 	}
 }
 
@@ -20,6 +30,16 @@ contract parent {
 	function nameYourChild (string _myChildName){
 		child myChild = child(yourChildAddress);
 		myChild.setChildName(_myChildName);
+	}
+
+	function setChildAge (uint256 _myChildAge){
+		child myChild = child(yourChildAddress);
+		myChild.setChildAge(_myChildAge);
+	}
+
+	function setChildLocation (string _myChildLocation){
+		child myChild = child(yourChildAddress);
+		myChild.setChildLocation(_myChildLocation);
 	}
 }
 
