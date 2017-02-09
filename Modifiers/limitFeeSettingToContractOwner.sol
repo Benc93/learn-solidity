0611431d10bb369848a3b6a02ce9fed52b09c6df
Ben Conway <bendavidconway@me.com>
2017-02-09 08:01:24 +0700

    first use of modifiers in parent contract to control functions in child contract

---
+++ Modifiers/limitFeeSettingToContractOwner.sol
@@ -0,0 +1,71 @@
+
+
+contract ownedByCryptoUni{
+	
+	string public uniName = "Crypto Uni";
+
+	address public owner;
+
+	function ownedByCryptoUni(){
+		owner = msg.sender;
+	}
+	// This function mutes the contract, as it cannot be deleted from the blockchain
+	function killTheContract(){
+		suicide(owner);
+	}
+
+	// Limits the use of fee setting to the contract owner in ethereumCourse
+	modifier onlyOwner{
+ 
+    if (msg.sender != owner){ // If the sender of the message is not the owner, throw.
+ 
+      throw;
+ 
+    }else{
+ 
+      _;
+    }
+ 
+  	}
+}
+
+
+// This contract will inherit all the variables and functionality of it's owner contract
+contract ethereumCourse is ownedByCryptoUni{
+
+	struct Student{
+		string studentName;
+		uint studentAge;
+		bool active;
+
+	}
+
+	uint256 public fee;
+
+	mapping(address=>Student) public myStudents;
+
+	// 1. Register to my school
+	// 2. Set fee for registration
+
+	function Register(
+		string _typeYourName,
+		uint _whatIsYourAge){
+
+		if (msg.value == fee){
+		// register the student
+		myStudents[msg.sender] = Student({
+			studentName: _typeYourName,
+			studentAge: _whatIsYourAge,
+			active: true
+			});		
+		}else{
+			throw;
+		}
+
+	}
+	// Allows contract owner to set the fee of the contract
+	function setRegistrationFee(uint256 _fee) onlyOwner {
+		fee = _fee;
+	}
+
+}
